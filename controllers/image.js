475cde1acc04aa088b2314b523bfad2adf6e7554
yangyang36 <9723006@nkust.edu.tw>
2022-09-23 01:22:36 +0800

    add dotenv

--- controllers/image.js
+++ controllers/image.js
@@ -1,9 +1,11 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai")
 
-//You must add your own API key here from Clarifai. 
+require("dotenv").config()
+
+//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE' 
-});
+  apiKey: process.env.CLARIFAI_API_KEY,
+})
 
 const handleApiCall = (req, res) => {
   app.models
@@ -16,29 +18,31 @@ const handleApiCall = (req, res) => {
     // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     // to:
     // .predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
+    // 'c0c0ac362b03416da06ab3fa36fb58e3'
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
-      res.json(data);
+    .then((data) => {
+      res.json(data)
     })
-    .catch(err => res.status(400).json('unable to work with API'))
+    .catch((err) => res.status(400).json("unable to work with API"))
 }
 
 const handleImage = (req, res, db) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-    // entries[0] --> this used to return the entries
-    // TO
-    // entries[0].entries --> this now returns the entries
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+  const { id } = req.body
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+      // entries[0] --> this used to return the entries
+      // TO
+      // entries[0].entries --> this now returns the entries
+      res.json(entries[0].entries)
+    })
+    .catch((err) => res.status(400).json("unable to get entries"))
 }
 
 module.exports = {
   handleImage,
-  handleApiCall
+  handleApiCall,
 }
